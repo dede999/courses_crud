4f7d77e1e6175f3eb2f89735cd1bec8061660dfd
André Luiz <andreluizsa00@gmail.com>
2025-07-10 10:33:06 -0300

    refactor: update user table structure and remove init.sql
    This commit modifies the user table to use UUID as the primary key and
    adjusts the timestamp types. Additionally, it removes the database
    initialization from the init.sql file, which contained unnecessary tables
    and functions.

---
+++ src/schema.rs
@@ -0,0 +1,15 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Uuid,
+        #[max_length = 255]
+        email -> Varchar,
+        #[max_length = 255]
+        password_hash -> Varchar,
+        #[max_length = 255]
+        name -> Varchar,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+    }
+}
